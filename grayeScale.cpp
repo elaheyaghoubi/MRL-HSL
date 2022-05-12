02b873ddf36bb13910e596475025f02f27d0c100
elahe yaghoubi <105369861+elaheyaghoubi@users.noreply.github.com>
2022-05-13 00:17:01 +0430

    Update grayeScale.cpp

--- grayeScale.cpp
+++ grayeScale.cpp
@@ -9,14 +9,25 @@ int main(){
 
     Mat input_img;
     Mat change_color;
+    char mode;
 
+    cout << "which mode do you want?(1=RGB , 2=GrayScale)\n";
+    cin >> mode;
     VideoCapture cap(0);
-    while (true) {
+    switch (mode){
+        case '1':
+        while (true) {
         cap >> input_img;
-        cvtColor(input_img,change_color,COLOR_RGB2GRAY);
-        imshow("GrayScale", change_color);
+        imshow("RGB", input_img);
             if(waitKey(5) >= 0) break;
         }
-
+        case '2':
+            while (true) {
+            cap >> input_img;
+            cvtColor(input_img,change_color,COLOR_RGB2GRAY);
+            imshow("GrayScale", change_color);
+            if(waitKey(5) >= 0) break;
+            }
+    }
     return 0;
 }
